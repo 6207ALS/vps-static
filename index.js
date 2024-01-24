f9b9415e7542ad389183d433f9f0aa342796f1c1
Allen Lee <6207ALS@gmail.com>
2024-01-24 18:52:48 -0500

    Adding Pug template engine

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const PORT = 3000
 
 app.use(express.json())
 app.use(express.static("public"))
+app.set("view engine", "pug")
+
+app.get("/", (req, res) => {
+	res.render("index")
+})
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}...`);
