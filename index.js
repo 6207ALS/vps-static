10bda2708663805f28d154639af83934c7a782be
Allen Lee <6207ALS@gmail.com>
2024-01-24 19:19:25 -0500

    Pug template engine

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000
 
 app.use(express.json())
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 app.set("views", __dirname + "/views")
 app.set("view engine", "pug")
 
